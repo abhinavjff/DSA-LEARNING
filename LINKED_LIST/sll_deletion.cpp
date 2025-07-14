d95170d8b0000cf447d9c9d189f300fa91319c24
Abhinav Kalyan Talluri <Abhinavkalyantalluri@gmail.com>
2025-07-14 14:49:13 +0530

    deleting a head ,tail,Kth postion and element

--- LINKED_LIST/sll_deletion.cpp
+++ LINKED_LIST/sll_deletion.cpp
@@ -1,16 +1,12 @@
 //going to start with deletion of a node in the singly linked list 
 /*
-linked lists store elements in a non-contiguous manner 
-LINKED lists applications - used in web browser
-
-they have 2 main features that define it 
-1)data
-2)link or pointer to the next element 
-
-so because we have 2 features we must have a user-defined data structure which will have both of them 
-so we need 'STRUCT'
-and a constructor to call it 
+SO IN DELETION OF THE LINKED LIST WE WILL LOOK INTO DELETING 
+1)HEAD 
+2)TAIL
+3)Kth POSITION 
+4)VALUE 
 */
+
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -76,18 +72,82 @@ Node* deleteTail(Node* head)
     return head;
 }
 
+Node* deleteK(Node* head,int k)
+{
+
+    if(head==NULL){return head;}
+    if(k==1)  
+    {
+        Node* temp=head;
+        head=head->next;
+        free(temp);
+        return head;
+    }
+    int count=0;
+    Node* temp=head;
+    Node* prev=NULL;
+
+    while(temp!=NULL)
+    {
+        count++;
+    if(count==k)
+    {
+        prev->next=prev->next->next;
+        free(temp);
+        break;
+    }
+    prev=temp;
+    temp=temp->next;
+    }
+    return head;    
+}
+
+Node* deleteElement(Node* head,int el)
+{
+
+    if(head==NULL){return head;}
+    if(head->data==el)  
+    {
+        Node* temp=head;
+        head=head->next;
+        free(temp);
+        return head;
+    }
+    Node* temp=head;
+    Node* prev=NULL;
+
+    while(temp!=NULL){
+    if(temp->data==el){
+        prev->next=prev->next->next;
+        free(temp);
+        break;
+        }
+    prev=temp;
+    temp=temp->next;
+    }
+    return head;    
+}
+
 int main()
 {
-    vector<int> arr={2,5,8,7};
+    vector<int> arr={2,5,8,7,9,10,12};
 
     Node* head=ConvertArrToLL(arr);
 
-    //REMOVE THE HEAD AND RETURN THE LINKED LIST ie 5,8,7
+    //REMOVE THE HEAD AND RETURN THE LINKED LIST ie 5,8,7,9,10,12
     head=deletehead(head);  
     print(head);
 
-    //REMOVE THE TAIL AND RETURN THE LINKED LIST ie 5,8
+    //REMOVE THE TAIL AND RETURN THE LINKED LIST ie 5,8,7,9,10
     head=deleteTail(head);
+    print(head);
+    
+    //REMOVE THE SECOND ELEMENT IN THE LINKED LIST AMONG  5,8,7,9,10 TO GET  5,7,9,10
+    head=deleteK(head,2);
+    print(head);
+
+    //REMOVE THE VALUE 9 TO GET 5,7,10
+    head=deleteElement(head,9);
     print(head);
     
 
