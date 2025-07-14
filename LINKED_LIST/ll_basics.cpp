2d57553801159845455968c64e30d4e2e5e08dd9
Abhinav Kalyan Talluri <Abhinavkalyantalluri@gmail.com>
2025-07-14 11:01:05 +0530

    basics of linked list

---
+++ LINKED_LIST/ll_basics.cpp
@@ -0,0 +1,52 @@
+/*
+linked lists store elements in a non-contiguous manner 
+LINKED lists applications - used in web browser
+
+they have 2 main features that define it 
+1)data
+2)link or pointer to the next element 
+
+so because we have 2 features we must have a user-defined data structure which will have both of them 
+so we need 'STRUCT'
+and a constructor to call it 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{ //can use CLASS ALSO
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+
+    public:
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+Node* ConvertArrToLL(vector<int> &arr){
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+    //iterate
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+
+    return head;
+}
+
+int main()
+{
+    vector<int> arr={2,5,8,7};
+    Node* head=ConvertArrToLL(arr);
+    cout<<head->data;
+}
